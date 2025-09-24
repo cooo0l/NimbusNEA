8087afcb6ab1a4dde9b5b9546550f5407c9e54a9
Ben Kazakevicius <benkazakev@gmail.com>
2025-09-24 20:44:07 +0100

    on board LED test

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,15 @@
 
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Hello, World!");
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, HIGH);  
+  delay(1000);                     
+  digitalWrite(LED_BUILTIN, LOW);   
+  delay(1000);          
+
 }
